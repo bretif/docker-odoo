421d5b42b0fe7af4fbde63058dc6515cf39879d0
Bertrand Rétif <br@phosphore.eu>
2014-10-06 17:01:01 +0200

    First working version 	modifié:         Dockerfile 	modifié:         odoo.conf 	modifié:         run_odoo.sh

--- run_odoo.sh
+++ run_odoo.sh
@@ -2,7 +2,7 @@
 
 [ "${AUTOSTART}" = 'True' -a -x /home/odoo/server/odoo.py ] || exit 0
 
-/home/odoo/server/odoo.py -c ./odoo.conf >> /home/odoo/server/odoo.log 2>&1
+su odoo -c "/home/odoo/server/odoo.py -c ./odoo.conf ${CMDLINE_PARAM}" >> /home/odoo/server/odoo.log 2>&1
 
 # Script should not exit unless seafile died
 while pgrep -f "odoo.py" 2>&1 >/dev/null; do
